5dd1dd06ec21cf86fecbdeeac898909663a5a9ba
circloph <circloph@gmail.com>
2022-12-16 12:04:25 +0600

    connected Swagger

--- src/main/java/seven/winds/studio/controller/UserController.java
+++ src/main/java/seven/winds/studio/controller/UserController.java
@@ -1,11 +1,14 @@
 package seven.winds.studio.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import seven.winds.studio.dto.UserRequestDto;
 import seven.winds.studio.dto.UserResponseDto;
 import seven.winds.studio.service.UserService;
 
+@Tag(name = "User", description = "The User API")
 @RestController
 public class UserController {
 
@@ -16,10 +19,13 @@ public class UserController {
         this.userService = userService;
     }
 
+//    @Operation(summary = "Get user by id", tags = "user")
     @GetMapping("/users/{id}")
     public UserResponseDto getUser(@PathVariable Long id) {
         return userService.getUser(id);
     }
+
+//    @Operation(summary = "Save user", tags = "user")
 
     @PostMapping("/users")
     public UserResponseDto saveUser(@RequestBody UserRequestDto user) {
